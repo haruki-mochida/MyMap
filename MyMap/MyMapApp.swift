55877ab0ca1affa7e7f4a5ab0edf970323f4f3b2
mochidaharuki <mochidaharuki@plise.co.jp>
2023-04-20 14:20:46 +0900

    Initial Commit

---
+++ MyMap/MyMapApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyMapApp.swift
+//  MyMap
+//
+//  Created by 持田晴生 on 2023/04/20.
+//
+
+import SwiftUI
+
+@main
+struct MyMapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
