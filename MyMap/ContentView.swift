8befb5fa8f2f0a1e2fadb5482ef26538bce3fe76
mochidaharuki <mochidaharuki@plise.co.jp>
2023-04-20 19:11:08 +0900

    Initial commit

--- MyMap/ContentView.swift
+++ MyMap/ContentView.swift
@@ -8,14 +8,26 @@
 import SwiftUI
 
 struct ContentView: View {
+    //入力中の文字列を保持する状態変数
+    @State var inputText: String = ""
+    // 検索キーワードを保持する状態変数
+    @State var displaySearchKey:String = ""
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        //垂直にレイアウト(縦方向にレイアウト)
+        VStack{
+            //テキストフィールド（文字入力）
+            TextField("キーワード", text: $inputText, prompt: Text("キーワードを入力してください"))
+            //入力が官僚されたとき
+                .onSubmit {
+                    displaySearchKey = inputText
+                }
+            //余白を追加
+                .padding()
+            
+            //マップを表示
+            MapView(searchKey: displaySearchKey)
         }
-        .padding()
     }
 }
 
---
+++ MyMap/MapView.swift
@@ -0,0 +1,73 @@
+//
+//  MapView.swift
+//  MyMap
+//
+//  Created by 持田晴生 on 2023/04/20.
+//
+
+import SwiftUI
+import MapKit
+
+struct MapView: UIViewRepresentable {
+    //検索キーワード
+    let searchKey: String
+    //表示するViewを作成するときに実行
+    func makeUIView(context: Context) -> MKMapView {
+        //MKMapViewのインスタンス生成
+        MKMapView()
+    }//makeUIView　ここまで
+    
+    //表示した　View　が更新されるたびに実行
+    func updateUIView(_ uiView: MKMapView, context: Context) {
+        //入力された文字をデバックエリアに表示
+        print("検索キーワード:\(searchKey)")
+        
+        //CLGeocoderインスタンスを生成
+        let geocoder = CLGeocoder()
+        
+        //入力された文字から位置情報を取得
+        geocoder.geocodeAddressString(
+            searchKey,
+            completionHandler: { (placemarks, error) in
+                //リクエストの結果が存在し、1件目の情報から位置情報を取り出す
+                if let placemarks,
+                   let firstPlacemarks = placemarks.first,
+                   let location = firstPlacemarks.location{
+                    
+                    //位置情報から緯度経度をtargetCoordinateに取り出す
+                    let targetCoordinate = location.coordinate
+                    
+                    //緯度経度をデバックエリアに表示
+                    print("緯度経度:\(targetCoordinate)")
+                    
+                    //MKPointAnnotation()インスタンスを生成し、ピンを作る
+                    let pin = MKPointAnnotation()
+                    
+                    //ピンを置く場所に緯度経度を設定
+                    pin.coordinate = targetCoordinate
+                    
+                    //ピンのタイトルを設定
+                    pin.title = searchKey
+                    
+                    //ピンを地図に置く
+                    uiView.addAnnotation(pin)
+                    
+                    //緯度経度を中心に半径500mの範囲を表示
+                    uiView.region = MKCoordinateRegion(
+                        center:targetCoordinate,
+                        latitudinalMeters: 500.0,
+                        longitudinalMeters: 500.0)
+                    
+                }//ifここまで
+                
+            })//geocoderここまで
+        
+    }//updateUIViewここまで
+    
+}// MapView　ここまで
+
+struct MapView_Previews: PreviewProvider {
+    static var previews: some View {
+        MapView(searchKey: "羽田空港")
+    }
+}
